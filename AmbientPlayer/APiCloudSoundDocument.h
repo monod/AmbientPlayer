6bb82620b3100d2ea12ad008e519317a23da38f7
Masato setoyama <setoyama60jp@gmail.com>
2012-10-10 18:02:12 +0900

    renamed and icloud classes

---
+++ AmbientPlayer/APiCloudSoundDocument.h
@@ -0,0 +1,13 @@
+//
+//  APiCloudSoundDocument.h
+//  AmbientPlayer
+//
+//  Created by 瀬戸山 雅人 on 2012/10/10.
+//  Copyright (c) 2012年 Veronica Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APiCloudSoundDocument : UIDocument
+
+@end
---
+++ AmbientPlayer/APiCloudSoundDocument.m
@@ -0,0 +1,13 @@
+//
+//  APiCloudSoundDocument.m
+//  AmbientPlayer
+//
+//  Created by 瀬戸山 雅人 on 2012/10/10.
+//  Copyright (c) 2012年 Veronica Software. All rights reserved.
+//
+
+#import "APiCloudSoundDocument.h"
+
+@implementation APiCloudSoundDocument
+
+@end
