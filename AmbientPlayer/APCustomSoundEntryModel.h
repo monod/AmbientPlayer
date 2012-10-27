87a0b2321f57ca3bf774bbe66932404d68aa721d
Masato setoyama <setoyama60jp@gmail.com>
2012-10-27 15:21:31 +0900

    Set up for using Core Data

---
+++ AmbientPlayer/APCustomSoundEntryModel.h
@@ -0,0 +1,21 @@
+//
+//  APCustomSoundEntryModel.h
+//  AmbientPlayer
+//
+//  Created by 瀬戸山 雅人 on 2012/10/27.
+//  Copyright (c) 2012年 Veronica Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface APCustomSoundEntryModel : NSManagedObject
+
+@property (nonatomic, retain) NSString * sound_file;
+@property (nonatomic, retain) NSString * image_file;
+@property (nonatomic, retain) NSNumber * latitude;
+@property (nonatomic, retain) NSNumber * longitude;
+@property (nonatomic, retain) NSString * name;
+
+@end
